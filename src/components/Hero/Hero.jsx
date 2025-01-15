c8d8426ff8e7139876813b2c43d0bab0b80c6174
Armine Muradyan <muradyan.armin13@gmail.com>
2025-01-15 14:41:37 +0400

    add Hero component and functional

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,18 @@
+import "./Hero.css";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="hero-wrapper">
+      <div className="paddings innerWidth flexCenter hero-container">
+        <div className="hero-left"></div>
+        <div className="flexCenter hero-right">
+          <div className="image-container">
+            <img src="./hero-image.png" alt="hero"></img>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
