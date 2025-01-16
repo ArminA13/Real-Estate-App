f38c9e5feda1a8721bcdea29bae1dc34881c4955
Armine Muradyan <muradyan.armin13@gmail.com>
2025-01-16 14:19:05 +0400

    add statistical date

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,3 +1,4 @@
+import CountUp from "react-countup";
 import "./Hero.css";
 import { HiLocationMarker } from "react-icons/hi";
 
@@ -16,13 +17,40 @@ const Hero = () => {
             </h1>
           </div>
           <div className="flexColStart hero-des">
-            <span>Find a variety of properties that suit you very easilty</span>
-            <span>Forget all difficulties in finding a residence for you</span>
+            <span className="secondaryText">
+              Find a variety of properties that suit you very easilty
+            </span>
+            <span className="secondaryText">
+              Forget all difficulties in finding a residence for you
+            </span>
           </div>
           <div className="flexCenter search-bar">
             <HiLocationMarker color="var(--blue)" size={25} />
             <input type="text" />
             <button className="button">Search</button>
+          </div>
+          <div className="flexCenter stats">
+            <div className="flexColStart stat">
+              <span>
+                <CountUp start={7500} end={9000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Premium Products</span>
+            </div>
+            <div className="flexColStart stat">
+              <span>
+                <CountUp start={1950} end={2020} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Happy Customers</span>
+            </div>
+            <div className="flexColStart stat">
+              <span>
+                <CountUp end={24} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Award Winnings</span>
+            </div>
           </div>
         </div>
         {/* right side  */}
